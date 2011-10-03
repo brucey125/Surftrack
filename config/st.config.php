72a874a7a21049a9df1dfc81f88287dd8c91f27b
Josh Bruce <brucey125@hotmail.co.uk>
2011-10-03 13:57:25 +0100

    Folder structure and file setup complete

---
+++ config/st.config.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Copyright 2011 Surftrack, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+ /**
+ * Configuration to MySQL database
+ */
+ 
+$config['mysql_ip_local']			= "localhost";
+$config['mysql_ip_remote']			= "184.154.135.162";
+$config['mysql_username']			= "root";
+$config['mysql_password']			= "root";
+$config['mysql_database']			= "surftrack";
+
+/**
+ * Hosting information and Domain, depends
+ * on who the host is and who's system
+ * Surftrack is on.
+ */
+
+$config['base_domain']				= "http://localhost/surftrack";
+$config['admin_email']				= "brucey@surftrack.co.uk";
+
+?>
